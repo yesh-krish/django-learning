02efa34755979e01b4e345dfd37485fc39144934
yesh-krish <krishnayeshas@gmail.com>
2026-07-03 20:37:56 +0300

    chapter 3-migrations

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
+from datetime import date
+
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
