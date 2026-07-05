67975d84f6c861a74538061a0e3126607ec29bd9
yesh-krish <krishnayeshas@gmail.com>
2026-07-05 21:58:10 +0300

    pages and urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-
+app_name = 'posts'
 urlpatterns = [
-    path('', views.posts_list, name='posts_list'),
+    path('', views.posts_list, name='list'),
+    path('<slug:slug>/', views.post_page, name='page')
+    ,path('id/<int:pk>/', views.post_page_by_pk, name='page_by_pk')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse
 
 def posts_list(request):
     posts = Post.objects.all().order_by('-date')
     return render(request, 'posts/posts_list.html', {'posts': posts})
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post_page.html', {'post': post})
+
+def post_page_by_pk(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'posts/post_page.html', {'post': post})
